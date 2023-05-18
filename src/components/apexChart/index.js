1f96287ec02a5e1a23926fb2cb35444a980efdca
Tomide Adeoye <tomide@dipfellows.org>
2023-05-19 00:09:14 +0100

    bar

--- src/components/apexChart/index.js
+++ src/components/apexChart/index.js
@@ -37,6 +37,7 @@ const ApexChart = () => {
 		chart: {
 			height: 350,
 			type: "candlestick",
+			id: "candles",
 			toolbar: {
 				autoSelected: "pan",
 				show: true,
@@ -58,90 +59,89 @@ const ApexChart = () => {
 				title: "dan",
 			},
 		},
-		tooltip: {
-			custom: function ({ series, seriesIndex, dataPointIndex, w }) {
-				return (
-					'<div class="arrow_box">' +
-					"<span>" +
-					//the whole series is offered as a prop
-					//plus the relevant series in the data if there are more than one
-					//plus the index of where this sits on the x axis
-					//plus a global object w to extract other chart data.
-					//apexcharts.js/src/modules/settings/Globals.js
-					//usw W for better access rather than series[seriesindex][datapointindex]
-					JSON.stringify(w.globals.categoryLabels[dataPointIndex]) +
-					"</span>" +
-					"</div>"
-				);
-			},
-		},
-		optionsBar: {
-			chart: {
-				// height: 160,
-				type: "bar",
-				// brush: {
-				// 	enabled: true,
-				// 	// target: "candle",
-				// },
-				selection: {
-					enabled: true,
-					xaxis: {
-						min: new Date("20 Jan 2017").getTime(),
-						max: new Date("10 Dec 2023").getTime(),
-					},
-					// fill: {
-					// 	color: "#ccc",
-					// 	opacity: 0.4,
-					// },
-					stroke: {
-						color: "#0D47A1",
-					},
-				},
-			},
-			dataLabels: {
-				enabled: false,
-			},
-			plotOptions: {
-				bar: {
-					columnWidth: "80%",
-					colors: {
-						ranges: [
-							{
-								from: -1000,
-								to: 0,
-								color: "#FF6838",
-							},
-							{
-								from: 1,
-								to: 10000,
-								color: "#58BD7D",
-							},
-						],
-					},
-				},
-			},
-			stroke: {
-				width: 9,
-				colors: "#FF6838",
-			},
-			xaxis: {
-				// type: "datetime",
-				// axisBorder: { offsetX: 13 },
-			},
-			yaxis: { labels: { show: true } },
-		},
+		// tooltip: {
+		// 	custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+		// 		return (
+		// 			'<div class="arrow_box">' +
+		// 			"<span>" +
+		// 			//the whole series is offered as a prop
+		// 			//plus the relevant series in the data if there are more than one
+		// 			//plus the index of where this sits on the x axis
+		// 			//plus a global object w to extract other chart data.
+		// 			//apexcharts.js/src/modules/settings/Globals.js
+		// 			//usw W for better access rather than series[seriesindex][datapointindex]
+		// 			JSON.stringify(w.globals.categoryLabels[dataPointIndex]) +
+		// 			"</span>" +
+		// 			"</div>"
+		// 		);
+		// 	},
+		// },
 	};
 
-	const seriesBar = [
-		{
-			name: "volume",
-			data: data?.map((data, index) => ({
-				x: data.x,
-				y: [data.y[0], data.y[1], data.y[0], data.y[1]],
-			})),
+	const optionsBar = {
+		series: [
+			{
+				name: "volume",
+				data: data?.map((data) => ({
+					x: data.x,
+					y: [data.y[3], data.y[0], data.y[3], data.y[0]],
+				})),
+			},
+		],
+		chart: {
+			brush: {
+				enabled: true,
+				target: "candles",
+			},
+			selection: {
+				enabled: true,
+				fill: {
+					color: "#ccc",
+					opacity: 0.4,
+				},
+				stroke: {
+					width: 8,
+					color: "#0D47A1",
+				},
+			},
 		},
-	];
-	console.log("sibar", seriesBar);
+		dataLabels: {
+			enabled: false,
+		},
+		stroke: {
+			width: 0,
+		},
+		plotOptions: {
+			bar: {
+				columnWidth: "30%",
+				colors: {
+					ranges: [
+						{
+							from: -900000000000000000000000000,
+							to: 0,
+							color: "#FF6838",
+						},
+						{
+							from: 1,
+							to: 900000000000000000000000000,
+							color: "#58BD7D",
+						},
+					],
+				},
+			},
+		},
+
+		xaxis: {
+			labels: {
+				show: false,
+			},
+		},
+		yaxis: {
+			labels: {
+				show: false,
+			},
+		},
+	};
 
 	return (
 		<Stack
@@ -159,15 +159,15 @@ const ApexChart = () => {
 							options={options}
 							series={series}
 							type="candlestick"
-							// height={350}
+							height={400}
 						/>
 					</div>
 					<div id="chart-bar">
 						<ReactApexChart
-							options={options.optionsBar}
-							series={seriesBar}
+							options={optionsBar}
+							series={optionsBar.series}
 							type="bar"
-							height={160}
+							height={100}
 						/>
 					</div>
 				</div>
